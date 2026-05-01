90833de6141e767ca7d929dafb07b370573e5398
tebogo60 <tjjmalebana@gmail.com>
2026-05-01 17:55:04 +0200

    feat: implement routing and add initial page components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
-export default function App() {
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import ForgotPassword from "./pages/ForgotPassword";
+import ResetPassword from "./pages/ResetPassword";
+
+function App() {
     return (
-        <div>
-            <h1>Cassino Frontend Running</h1>
-        </div>
+        <Routes>
+            <Route path="/"></Route>
+            <Route path="/home"></Route>
+            <Route path="/login"></Route>
+            <Route path="/register"></Route>
+            <Route path="/forgot-password"></Route>
+            <Route path="/reset-password"></Route>
+        </Routes>
     );
 }
+
+export default App;
